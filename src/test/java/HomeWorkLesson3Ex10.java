b221cc4e12353c3a7c9ace53f54b9fae1a86dbc4
Aleksandr Bashkov <banjet@inbox.ru>
2022-11-23 09:24:40 +0300

    initial commit

---
+++ src/test/java/HomeWorkLesson3Ex10.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HomeWorkLesson3Ex10 {
+
+        @Test
+        public void testAssertHome() {
+            Response response = RestAssured
+                    .get("https://playground.learnqa.ru/api/map")
+                    .andReturn();
+
+            assertEquals(response.getStatusCode(), 200,
+                    "Status code should be 200");
+
+            char[] chars = response.getBody().toString().toCharArray();
+            assertTrue(chars.length > 15,
+                    "Length of response less then 15 symbols");
+
+        }
+
+    }
---
+++ src/test/java/HomeWorkLesson3Ex11.java
@@ -0,0 +1,27 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HomeWorkLesson3Ex11 {
+
+        @Test
+        public void testCookieHome() {
+            Response response = RestAssured
+                    .get("https://playground.learnqa.ru/api/homework_cookie")
+                    .andReturn();
+
+            assertEquals(response.getStatusCode(), 200,
+                    "Status code should be 200");
+
+            Map<String, String> cookies = response.getCookies();
+            HashMap<String, String> expected = new HashMap<>();
+            expected.put("HomeWork", "hw_value");
+            assertEquals(expected, cookies);
+        }
+
+    }
